be8050c0bd25efc87a20951180946f91e309b459
Salman <18salmanz@gmail.com>
2023-09-02 09:09:43 +0530

    dd inital dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Vite + React + TypeScript + Husky = 🔥</p>
-        <p>
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          {' | '}
-          <a className="App-link" href="https://github.com/typicode/husky" target="_blank" rel="noopener noreferrer">
-            Husky Docs
-          </a>
-          {' | '}
-          <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noopener noreferrer">
-            React Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="flex justify-center">Weather App! ☁🌡</div>;
+};
 
 export default App;
