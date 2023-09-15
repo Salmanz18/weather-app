029013a6874bde68a9060da5552bde0b1402be94
Salmanz18 <18salmanz@gmail.com>
2023-09-16 00:49:57 +0530

    update url

--- src/utils/BaseUrl.ts
+++ src/utils/BaseUrl.ts
@@ -1,2 +1,2 @@
 export const baseUrl = 'https://api.openweathermap.org/data/2.5';
-export const baseUrlGeo = 'http://api.openweathermap.org/geo/1.0';
+export const baseUrlGeo = 'https://api.openweathermap.org/geo/1.0';
